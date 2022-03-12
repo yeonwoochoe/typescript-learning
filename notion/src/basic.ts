ac00265df9ff6a6e5873234fc042903c278091ad
yeonwoo <chiyy6162@gmail.com>
2022-03-12 18:14:44 +0900

    [feat] TIL : 원시타입형 변수 설정해보기

---
+++ notion/src/basic.ts
@@ -0,0 +1,17 @@
+//원시타입 : 숫자, 문자, 불리언
+//객체,배열
+//함수
+
+let age: number;
+//타입을 명칭할때는 반드시 소문자로 형태로 명시해야 한다.
+// 앞 첫글자를 대문자로 명시할 시 자바스크립트의 객체로 인식하기 때문.
+
+age = 30;
+
+let userName: string;
+
+userName = "yeonwoo";
+
+let isInstructor: boolean;
+
+isInstructor = true;

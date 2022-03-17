9d08c11132f9886ba4992cdde7fc70766ee78ca2
yeonwoo <chiyy6162@gmail.com>
2022-03-17 14:53:29 +0900

    [feat] ts : ref 및 state 상태관리

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,7 +1,17 @@
-import "./App.css";
+import NewTodo from "./components/NewTodo";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
-  return <div></div>;
+  const todos = [new Todo("리액트 배우기"), new Todo("타입스크립트 배우기")];
+
+  const addTodoHandler = (todoText: string) => {};
+  return (
+    <div>
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} />
+    </div>
+  );
 }
 
 export default App;
